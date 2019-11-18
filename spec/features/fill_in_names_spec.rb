a76886828a8fc56b4167fb41ee34106475b1495f
djlonboy <djlonboy@gmail.com>
2019-11-18 16:06:32 +0000

    Passed test for displaying players names

--- spec/features/fill_in_names_spec.rb
+++ spec/features/fill_in_names_spec.rb
@@ -4,7 +4,7 @@ feature 'players can enter names' do
   scenario 'and returns names on next screen' do
     visit('/')
     fill_in "player1", with: "Rob"
-    fill_in "player_2", with: "John"
+    fill_in "player2", with: "John"
     click_button "submit"
     expect(page).to have_content "Names: Rob & John"
   end
