69bd7180d4b6efb655a34c477f8d2594afb8499d
djlonboy <djlonboy@gmail.com>
2019-11-21 11:45:58 +0000

    Refactored game_spec unit tests:  - Tests now use doubles instead of instances of Player class

--- lib/game.rb
+++ lib/game.rb
@@ -5,11 +5,18 @@ class Game
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @turn = 1
   end
 
   def attack(player)
     player.reduce_points(ATTACK_POINTS)
     "#{player.name} was attacked!<br>Current hit points: #{player.hit_points}"
+  end
+
+  def turn
+    @turn = 2 if @turn == 1
+    @turn = 1 if @turn == 2
+    @turn
   end
 
 end
--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -3,15 +3,20 @@ require './lib/player'
 
 describe Game do
 
+  before(:each) do
+    @player1 = double("Player Class")
+    allow(@player1).to receive(:name).and_return "player1"
+    allow(@player1).to receive(:hit_points).and_return 50
+    allow(@player1).to receive(:reduce_points).with(10)
+  end
+
   it "accepts two players when created" do
     expect(Game).to respond_to(:new).with(2).arguments
   end
 
   it "can register an attack" do
-    player1 = Player.new("Rob")
-    game = Game.new(player1, :player2)
-    expect(game.attack(player1)).to include("Rob was attacked!")
+    game = Game.new(@player1, @player1)
+    expect(game.attack(@player1)).to include("player1 was attacked!")
   end
-
 
 end
