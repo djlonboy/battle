ec2731c66c73293718ab261092b4a1777c3f6023
djlonboy <djlonboy@gmail.com>
2019-11-21 13:46:51 +0000

    Added unit test for turn switcher

--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,7 @@ class Game
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @turn = 1
+    @turn = "Player 1"
   end
 
   def attack(player)
--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -8,6 +8,11 @@ describe Game do
     allow(@player1).to receive(:name).and_return "player1"
     allow(@player1).to receive(:hit_points).and_return 50
     allow(@player1).to receive(:reduce_points).with(10)
+
+    @player2 = double("Player Class")
+    allow(@player2).to receive(:name).and_return "player1"
+    allow(@player2).to receive(:hit_points).and_return 50
+    allow(@player2).to receive(:reduce_points).with(10)
   end
 
   it "accepts two players when created" do
@@ -19,4 +24,10 @@ describe Game do
     expect(game.attack(@player1)).to include("player1 was attacked!")
   end
 
+  it ": Turn method switches between players" do
+    game = Game.new(@player1, @player2)
+    expect(game.turn).to eq "Player 2"
+    expect(game.turn).to eq "Player 1"
+  end
+  
 end
