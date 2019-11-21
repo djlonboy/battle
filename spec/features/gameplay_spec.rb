16bfde6a65c43666728b7bcb8f4b71f57862df98
djlonboy <djlonboy@gmail.com>
2019-11-21 13:52:33 +0000

    Added feature tests for switching turns

--- spec/features/gameplay_spec.rb
+++ spec/features/gameplay_spec.rb
@@ -10,7 +10,7 @@ end
 feature 'players can check eachothers points:' do
   scenario 'Player 1 can check player 2s hit points' do
     sign_in_and_play
-      expect(page).to have_content "John hit points: 50"
+    expect(page).to have_content "John hit points: 50"
   end
 end
 
@@ -19,5 +19,20 @@ feature 'players can attack one another:' do
     sign_in_and_play
     click_button 'Attack'
     expect(page).to have_content "John was attacked!"
+  end
+end
+
+feature 'turns switch between players' do
+  scenario 'Turn passes to player 2 after player 1' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content "John's turn"
+  end
+
+  scenario 'Turn passes to player 2 after player 1' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'Attack'
+    expect(page).to have_content "Rob's turn"
   end
 end
